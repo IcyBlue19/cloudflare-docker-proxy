97eaa472058c9f0860780dcd3f0b04f4f36c4242
IcyBlue19 <156685034+IcyBlue19@users.noreply.github.com>
2024-06-08 08:51:42 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker-registry.ick.moe": "https://registry-1.docker.io",
+  "quay.ick.moe": "https://quay.io",
+  "gcr.ick.moe": "https://gcr.io",
+  "k8s-gcr.ick.moe": "https://k8s.gcr.io",
+  "k8s.ick.moe": "https://registry.k8s.io",
+  "ghcr.ick.moe": "https://ghcr.io",
+  "cloudsmith.ick.moe": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
